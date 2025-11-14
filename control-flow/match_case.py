d75aaf81ee6803a2248818cabde484106f310e1c
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2025-11-14 11:31:37 +0100

    add weather advice program in control flow

---
+++ control-flow/match_case.py
@@ -0,0 +1,9 @@
+match expression:
+    case pattern1:
+        code_block_1
+    case pattern2:
+        code_block_2
+    ...
+    case pattern_n:
+        code_block_n
+    # Optional: _ (wildcard) for default case
