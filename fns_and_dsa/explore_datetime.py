d82162dd7729e3cf24b6c8d436612004511f608a
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2025-11-22 00:44:11 +0100

    format future date

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -8,4 +8,4 @@ def display_current_datetime():
 def calculate_future_date():
     no_of_days = int(input("Enter a number of days: "))
     future_date = datetime.now() + timedelta(days(no_of_days))
-    print(future_date)
+    print(future_date.strftime("%Y-%m-%d %H:%M:%S"))
