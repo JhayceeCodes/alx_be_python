7111e1f697b05da5bc1923f3f8a585bb026dbe7a
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2025-11-30 21:59:22 +0100

    rename subtraction test method

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -13,7 +13,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-1, 1), 0)
         # Add more assertions to thoroughly test the add method.
     
-    def test_subtract(self):
+    def test_subtraction(self):
         """Test the subtraction method."""
         self.assertEqual(self.calc.subtract(5, 2), 3)
         self.assertEqual(self.calc.subtract(3, 3), 0)
