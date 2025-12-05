d59a678869a8f36763744d6761b428996e1a9faf
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2025-12-05 20:44:14 +0100

    fix: add __str__ method to each class

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,16 +2,22 @@ class Book:
     def __init__(self, title:str, author:str):
         self.title = title
         self.author = author
+    def __str__(self):
+        return "Book"
 
 class EBook(Book):
     def __init__(self, title, author, file_size:int):
         super().__init__(title, author)
         self.file_size = file_size
+    def __str__(self):
+        return "EBook"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count:int):
         super().__init__(title, author)
         self.page_count = page_count
+    def __str__(self):
+        return "PrintBook"
     
 class Library:
     def __init__(self, books=[]):
@@ -22,7 +28,7 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            book_type = book.__class__.__name__
+            
             """if book_type == "Book":
                 print(f"Book: {book.title} by {book.author}")
             elif book_type == "EBook":
@@ -31,7 +37,7 @@ class Library:
                 print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
             else:
                 print("Book type not found.")"""
-            match book_type:
+            match str(book):
                 case "Book":
                     print(f"Book: {book.title} by {book.author}")
                 case "EBook":
